7cb1ea27694d1e31f0c96d1ebc263cd96b3d0bb5
Hector Orellana <hector@zooft.company>
2023-03-25 21:12:55 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+enum Instructions {
+    Exit,
+    Chicken,
+    Add,
+    Subtract,
+    Multiply,
+    Compare,
+    Load,
+    Store,
+    Jump,
+    Char
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+
+#[cfg(test)]
+mod tests {
+}
